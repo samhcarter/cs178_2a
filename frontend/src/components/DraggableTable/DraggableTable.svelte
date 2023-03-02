3826c30c86e3d84d86fc0685f388f427123db647
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-03-02 00:00:28 -0500

    add interval for table

---
+++ frontend/src/components/DraggableTable/DraggableTable.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Button, { Group, Label } from '@smui/button';
+	const minuteIntervals = [1, 5, 10, 15, 30, 60];
+	let selectedInterval = minuteIntervals[4];
+</script>
+
+<div class="tableContainer">
+	<h1 class="tableTitle">DraggableTable</h1>
+	<Group>
+		{#each minuteIntervals as interval}
+			<Button
+				variant="raised"
+				color={selectedInterval === interval ? 'primary' : 'secondary'}
+				on:click={() => (selectedInterval = interval)}><Label>{interval}</Label></Button
+			>
+		{/each}
+	</Group>
+	<table>
+		<tr>
+			<th />
+			<th />
+		</tr>
+	</table>
+</div>
+
+<style>
+	.tableContainer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 75vh;
+		background-color: black;
+		margin: 10vw;
+	}
+	.tableTitle {
+		color: white;
+		font-size: 2rem;
+	}
+</style>
