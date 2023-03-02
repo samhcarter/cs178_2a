3b819f2561a7b481cdfa600de144bb04d94ee11c
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-03-02 15:41:28 -0500

    add table

--- frontend/src/components/DraggableTable/DraggableTable.svelte
+++ frontend/src/components/DraggableTable/DraggableTable.svelte
@@ -1,7 +1,30 @@
 <script>
 	import Button, { Group, Label } from '@smui/button';
+	import TimeRow from './components/TimeRow.svelte';
+	import moment from 'moment';
+
 	const minuteIntervals = [1, 5, 10, 15, 30, 60];
 	let selectedInterval = minuteIntervals[4];
+	let times = Array(1440 / selectedInterval + 1).fill(0);
+	let namedTimes = times.map((_, i) =>
+		moment('12:00 AM', 'hh:mm A')
+			.add(i * selectedInterval, 'minutes')
+			.format('hh:mm A')
+	);
+	$: if (selectedInterval) {
+		times = Array(1440 / selectedInterval + 1).fill(0);
+		namedTimes = times.map((_, i) =>
+			moment('12:00 AM', 'hh:mm A')
+				.add(i * selectedInterval, 'minutes')
+				.format('hh:mm A')
+		);
+	}
+	const getTime = (factor = 0) => {
+		return moment('12:00 AM', 'hh:mm A')
+			.add(factor * selectedInterval, 'minutes')
+			.format('hh:mm A');
+	};
+	getTime();
 </script>
 
 <div class="tableContainer">
@@ -15,25 +38,39 @@
 			>
 		{/each}
 	</Group>
-	<table>
-		<tr>
-			<th />
-			<th />
-		</tr>
-	</table>
+	<div class="vertical-space" />
+	<div class="timeContainer">
+		{#each namedTimes as time}
+			<TimeRow {time} />
+		{/each}
+	</div>
 </div>
 
 <style>
+	.vertical-space {
+		min-height: 20px;
+		flex-shrink: 0;
+	}
 	.tableContainer {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		height: 75vh;
-		background-color: black;
+		height: 90vh;
 		margin: 10vw;
 	}
 	.tableTitle {
 		color: white;
 		font-size: 2rem;
+	}
+	.timeContainer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100%;
+		width: 100%;
+		overflow: scroll;
+		padding-top: 2vh;
+		padding-bottom: 2vh;
+		border: 1px solid black;
 	}
 </style>
---
+++ frontend/src/components/DraggableTable/components/TimeRow.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let time = '00:00';
+</script>
+
+<div class="timeRow">
+	<td class="timeCell">{time}</td>
+	<td class="timeBlockCell" />
+</div>
+
+<style>
+	.timeRow {
+		display: flex;
+		flex: 1;
+		flex-direction: inline-flex;
+		width: 50%;
+		height: auto;
+	}
+	.timeCell {
+		background-color: transparent;
+		display: flex;
+		flex-shrink: 0;
+		justify-content: end;
+		padding-right: 10px;
+		position: relative;
+		bottom: 50%;
+	}
+	.timeBlockCell {
+		background-color: red;
+		display: flex;
+		flex: 2;
+		border: 1px solid black;
+	}
+	.timeRow:last-of-type > .timeBlockCell {
+		border: none;
+		background-color: transparent;
+	}
+</style>
